7d1aa625f5ccc7c650ae971e6212e29aad17d87d
mokhanko <co41986@gmail.com>
2019-09-08 14:35:57 +0300

    add simple server for stripe payments

--- client/src/components/stripeButton/index.js
+++ client/src/components/stripeButton/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = process.env.REACT_APP_STRIPE_KEY;
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful')
+    axios({
+      url : 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.warn('Some payment error', JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please sure you use th provided credit card'
+      );
+    })
   };
 
   return (
